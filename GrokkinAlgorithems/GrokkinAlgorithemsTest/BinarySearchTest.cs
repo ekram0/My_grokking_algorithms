d7d59681e7d63f7715bd5f064c8779463daf5d96
Ekram Mohamed <ekram010@hotmail.com>
2021-02-10 18:13:46 +0200

    add BinarySearch

---
+++ GrokkinAlgorithems/GrokkinAlgorithemsTest/BinarySearchTest.cs
@@ -0,0 +1,40 @@
+using My_Implement.IntroductionToAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace GrokkinAlgorithemsTest
+{
+    public class BinarySearchTest
+    {
+        [Fact]
+        public void IsEqual_ValueInRange_returnTrue()
+        {
+            //Act
+            var data = Enumerable.Range(0,10).ToArray();
+            var item = 3;
+            //Arrange
+            var result = BinarySearch.binarySearch(data, item);
+            var expected = 3;
+            //Assert
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void IsEqual_ValueOutRange_returnTrue()
+        {
+            //Act
+            var data = Enumerable.Range(5, 100).ToArray();
+            var item = 804;
+            //Arrange
+            var Actual = BinarySearch.binarySearch(data, item);
+            var Expected = Array.FindIndex(data, x => x == item);
+            //Assert
+            Assert.Equal(Expected, Actual);
+        }
+
+
+
+
+    }
+}
---
+++ GrokkinAlgorithems/My_Implement/01IntroductionToAlgorithms/BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace My_Implement.IntroductionToAlgorithms
+{
+    public class BinarySearch
+    {
+        public static int binarySearch(int[] list, int item)
+        {
+            var Low = 0;
+            var High = list.Length - 1;
+            if (!list.Contains(item)) return -1;
+            while (Low <= High)
+            {
+                int mid = (Low + High) / 2;
+                var guss = list[mid];
+                switch (guss)
+                {
+                    case var t when guss == item: return mid;
+                    case var t when guss > item: High = mid - 1; break;
+                    default: Low = mid + 1;break;
+                }
+
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            var data = Enumerable.Range(0, 1000).ToArray();
+            var item = 804;
+            var target = binarySearch(data, item);
+            Console.WriteLine(target);
+            Console.ReadKey();
+        }
+    }
+}
